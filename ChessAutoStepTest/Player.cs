fd6eb9d971f068fc81a268c13bfd693472cf0487
cymheart <cymheart@qq.com>
2019-10-17 10:56:42 +0800

    ok

---
+++ ChessAutoStepTest/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessAutoStepTest
+{
+    public class Player
+    {
+        public LinkedList<int> chessBoardPiecePos = new LinkedList<int>();
+
+
+        void Remove(int rowIdx, int colIdx)
+        {
+            int key = GetPieceKey(rowIdx, colIdx);
+
+        }
+
+        int GetPieceKey(int rowIdx, int colIdx)
+        {
+            return (rowIdx << 12) | colIdx;
+        }
+    }
+}
---
+++ ChessAutoStepTest/chessboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessAutoStepTest
+{
+    public class Chessboard
+    {
+        int rowCount;
+        int colCount;
+
+        Piece[,] boardPieces;
+
+        public Chessboard()
+        {
+
+        }
+
+        public void Create()
+        {
+            boardPieces = new Piece[rowCount, colCount];
+        }
+
+    }
+}
